90625ab31c77348940a11eadd84395e42dbc7f3d
Steve Gale <stevegale54@gmail.com>
2024-06-28 23:06:04 +0100

    initial commit, working web page

---
+++ src/main.cpp
@@ -0,0 +1,216 @@
+/* 
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-web-server-websocket-sliders/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+
+// Replace with your network credentials
+//const char* ssid = "hackspace-wifi";
+//const char* password = "funkytown1234";
+const char* ssid = "Thegales";
+const char* password = "talk2thegales";
+int brightness = 0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+// Create a WebSocket object
+
+AsyncWebSocket ws("/ws");
+// Set LED GPIO
+const int ledPin1 = 12;
+const int ledPin2 = 13;
+const int ledPin3 = 14;
+
+String message = "";
+String sliderValue1 = "0";
+String sliderValue2 = "0";
+String sliderValue3 = "0";
+
+int dutyCycle1;
+int dutyCycle2;
+int dutyCycle3;
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel1 = 0;
+const int ledChannel2 = 1;
+const int ledChannel3 = 2;
+
+const int resolution = 8;
+
+//Json Variable to Hold Slider Values
+JSONVar sliderValues;
+
+//Get Slider Values
+String getSliderValues(){
+  sliderValues["sliderValue1"] = String(sliderValue1);
+  sliderValues["sliderValue2"] = String(sliderValue2);
+  sliderValues["sliderValue3"] = String(sliderValue3);
+
+  String jsonString = JSON.stringify(sliderValues);
+  return jsonString;
+}
+
+// Initialize LittleFS
+int initFS() {
+  int brightness = 0;
+  if (!LittleFS.begin()) {
+    //Serial.println("An error has occurred while mounting LittleFS");
+    brightness = -1;
+  }
+  else{
+   //Serial.println("LittleFS mounted successfully");
+  
+   File file = LittleFS.open("/brightness.txt");
+    if(!file){
+      //Serial.println("Failed to open file for reading");
+      brightness = -2;
+    }
+  
+    //Serial.println("File Content:");
+    char myString[10];
+    int index = 0;
+    while(file.available()){
+     
+      myString[index] = file.read();
+      index ++;
+    }
+    //Serial.print(myString);
+    brightness = atoi(myString);
+    if (brightness > 100){
+      brightness = 100;
+    }
+    else if (brightness < 0){
+      brightness = 0;
+    }
+   }
+   
+
+   return brightness;
+}
+void writeBrightness(int value){
+  File file = LittleFS.open("/brightness.txt", "w");
+  char numberArray[10];
+   itoa(value, numberArray,8);
+  file.print(numberArray);
+  file.close();
+}
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void notifyClients(String sliderValues) {
+  ws.textAll(sliderValues);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    message = (char*)data;
+    if (message.indexOf("1s") >= 0) {
+      sliderValue1 = message.substring(2);
+      writeBrightness(sliderValue1.toInt());
+      dutyCycle1 = map(sliderValue1.toInt(), 0, 100, 0, 255);
+      Serial.println(dutyCycle1);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }
+    if (message.indexOf("2s") >= 0) {
+      sliderValue2 = message.substring(2);
+      dutyCycle2 = map(sliderValue2.toInt(), 0, 100, 0, 255);
+      Serial.println(dutyCycle2);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }    
+    if (message.indexOf("3s") >= 0) {
+      sliderValue3 = message.substring(2);
+      dutyCycle3 = map(sliderValue3.toInt(), 0, 100, 0, 255);
+      Serial.println(dutyCycle3);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }
+    if (strcmp((char*)data, "getValues") == 0) {
+      notifyClients(getSliderValues());
+    }
+  }
+}
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  brightness = initFS();
+  if (brightness <=0){
+    Serial.println("error with file");
+  }
+  else {
+    Serial.print("stored brightness is ");
+    Serial.println(brightness);
+  }
+  initWiFi();
+  sliderValue1 = brightness;
+  dutyCycle1 = map(sliderValue1.toInt(), 0, 100, 0, 255);
+ 
+  // Set up LEDC pins
+  ledcAttachChannel(ledPin1, freq, resolution, ledChannel1);
+  ledcAttachChannel(ledPin2, freq, resolution, ledChannel2);
+  ledcAttachChannel(ledPin3, freq, resolution, ledChannel3);
+
+  initWebSocket();
+  
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+  
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  ledcWrite(ledPin1, dutyCycle1);
+  ledcWrite(ledPin2, dutyCycle2);
+  ledcWrite(ledPin3, dutyCycle3);
+
+  ws.cleanupClients();
+}
